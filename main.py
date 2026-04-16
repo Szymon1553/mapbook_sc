1d058c521b2101821ed2139c96dfe2b5d3dac69d
Szymon1553 <szymon.cichon@student.wat.edu.pl>
2026-04-16 13:19:24 +0200

    wip: complex data structure

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-zmienna_1: str = "oliwia"
-zmienna_2: str = "paweł"
-zmienna_3: str = "elizka"
-zmienna_4: str = "filip"
-zmienna_5: str = "ewelina"
-zmienna_6: str = "weronika"
 
-users: list = [zmienna_1, zmienna_2, zmienna_3, zmienna_4, zmienna_5, zmienna_6]
 
-for user in users:
-    print(user)
+users: list = [
+    {"username": "oliwia", "location": "łódź", "posts": "1", "usermessage": ["kocham legie", "sprzedam opla", "kiwi"]},
+    {"username": "paweł", "location": "ostróda", "posts": "2", "usermessage": ["kocham legie1", "sprzedam opla1", ]},
+    {"username": "eliza", "location": "radom", "posts": "3", "usermessage": ["kocham legie2", ]},
+    {"username": "filip", "location": "dęblin", "posts": "4", "usermessage": ["kocham legie3", "sprzedam opla3", "kiwi3"]},
+]
+
+for user in users[1:]:
+    print(f"Twój znajomy {user['username']} z miejscowości {user['location']} opublikował {user['posts']} wiadomości. Ostatnia wiadomość: {user['usermessage'][-1]}")
+
+#     Twój znajomy Filip z miejscowości Dęblin opublikował jeden post o treści: zyczenia
